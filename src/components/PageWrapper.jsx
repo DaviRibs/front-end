c7557fa4eb423a5aaf0251025b32c5f280dbfa20
DaviRibs <davirb21@gmail.com>
2025-06-10 08:13:28 -0300

    ajust/usereffect/token

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react"
+
 export default function PageWrapper({children}){
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if(!token)[
+            window.location.href = '/'
+        ]
+    })
     return (
         <div className="w-full h-screen bg-white text-black">
             {children}
